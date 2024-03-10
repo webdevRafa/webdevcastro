638c647cb6b74a98c2c71364ff008c39b0cdbecd
Rafael Castro <ralphc1992@live.com>
2024-03-09 19:24:00 -0600

    some tweaks

--- src/Components/Parallaxer.tsx
+++ src/Components/Parallaxer.tsx
@@ -1,24 +1,12 @@
 import { Parallax } from "react-parallax";
-import scissors from "../imgs/scissors.jpg";
+import cutter from "../imgs/cutter.jpg";
 const Parallaxer = () => {
   return (
     <>
-      <Parallax bgImage={scissors} strength={500}>
-        <div style={{ height: 500 }}>
-          <div
-            style={{
-              position: "absolute",
-              top: "50%",
-              left: "50%",
-              transform: "translate(-50%, -50%)",
-            }}
-          >
-            <h1 className="text-white text-4xl font-Josefin">
-              QUALITY SERVICE & GREAT VIBES
-            </h1>
-          </div>
-        </div>
-      </Parallax>
+      <div className="w-full h-full">
+        <img src={cutter} alt="" />
+      </div>
+      <div className="h-[600px]"></div>
     </>
   );
 };
